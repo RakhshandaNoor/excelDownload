6865ff7c3d6a241ec522fe3b575f389fc1940bf8
RakhshandaNoor <rakhshanda2396@hotmail.com>
2021-02-24 18:05:38 +0530

    first commit

---
+++ src/main/java/com/example/demo/vpmn/VpmnController.java
@@ -0,0 +1,70 @@
+package com.example.demo.vpmn;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class VpmnController {
+	
+	@GetMapping("/downloads")
+	public ResponseEntity downloadVmpn() throws IOException {
+		
+		 HttpHeaders headers = new HttpHeaders();
+	        headers.add("Content-Disposition", "attachment; filename=vpmn.xlsx");
+	        
+	        ByteArrayInputStream res = getVpmnReport();
+		return ResponseEntity.ok().headers(headers).body(new InputStreamResource(res));
+	}
+	
+	private ByteArrayInputStream getVpmnReport() throws IOException{
+		String[] COLUMNs = {"Id", "Name", "Address", "Age"};
+	    try(
+	        Workbook workbook = new XSSFWorkbook();
+	        ByteArrayOutputStream out = new ByteArrayOutputStream();
+	    ){
+	      CreationHelper createHelper = workbook.getCreationHelper();
+	   
+	      Sheet sheet = workbook.createSheet("Customers");
+	   
+	      Font headerFont = workbook.createFont();
+	      headerFont.setBold(true);
+	      headerFont.setColor(IndexedColors.BLUE.getIndex());
+	   
+	      CellStyle headerCellStyle = workbook.createCellStyle();
+	      headerCellStyle.setFont(headerFont);
+	   
+	      // Row for Header
+	      Row headerRow = sheet.createRow(0);
+	   
+	      // Header
+	      for (int col = 0; col < COLUMNs.length; col++) {
+	        Cell cell = headerRow.createCell(col);
+	        cell.setCellValue(COLUMNs[col]);
+	        cell.setCellStyle(headerCellStyle);
+	      }
+		
+	      workbook.write(out);
+	      return new ByteArrayInputStream(out.toByteArray());
+	    }
+	}
+
+}
